f8ba3d6874d8c6c0fa7efa62202408747ce385f3
Jan Rother <janrother02@gmail.com>
2024-11-25 05:55:06 +0100

    fix(script): fix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 ############################################################
 ########## Dotfiles for Toolchain-as-Code Project ##########
@@ -13,38 +14,78 @@
 GITHUB_USER=toolchain-as-code
 GITHUB_REPO=tac-dotfiles
 
+DOTFILES_ARCHIVE=/tmp/${GITHUB_REPO}.tar.gz
 DOTFILES_DIR=/tmp/${GITHUB_REPO}
+TARGET_DIR=${HOME}
 
 # |------------------------ Helper ------------------------|
 
 get_dotfiles() {
-  echo "      - Creating temporary directory."
-  mkdir -p /tmp/
+  echo "      - Creating temporary directory:"
+  mkdir -p "${DOTFILES_DIR}" || {
+    echo "        ! ERROR: Failed to create temporary directory: ${DOTFILES_DIR}"
+    exit 1
+  }
+  echo "          ${DOTFILES_DIR}"
+
   echo "      - Downloading dotfiles."
-  curl -#fLo /tmp/${GITHUB_REPO}.tar.gz "https://github.com/${GITHUB_USER}/${GITHUB_REPO}/tarball/main"
+  curl -#fLo "${DOTFILES_ARCHIVE}" "https://github.com/${GITHUB_USER}/${GITHUB_REPO}/tarball/main" || {
+    echo "        ! ERROR: Failed to download dotfiles archive."
+    exit 1
+  }
+
   echo "      - Extracting dotfiles."
-  tar -xzf /tmp/${GITHUB_REPO}.tar.gz --strip-components 1 -C /tmp/${GITHUB_REPO}
+  tar -xzf "${DOTFILES_ARCHIVE}" --strip-components 1 -C "${DOTFILES_DIR}" || {
+    echo "        ! ERROR: Failed to extract dotfiles archive."
+    exit 1
+  }
+
   echo "      - Removing dotfiles archive."
-  rm -f /tmp/${GITHUB_REPO}.tar.gz
+  rm -f "${DOTFILES_ARCHIVE}" || {
+    echo "        ! ERROR: Failed to remove dotfiles archive."
+    exit 1
+  }
 }
 
 install_dotfiles() {
   echo "      - Changing to dotfiles directory."
-  cd "${DOTFILES_DIR}/configs"
+  cd "${DOTFILES_DIR}/configs" || {
+    echo "          ERROR: 'configs/' directory of dotfiles not found."
+    exit 1
+  }
+
   echo "      - Installing dotfiles:"
-  find . -type f -exec sh -c 'cp -v "$1" "${HOME}/${1#./}" | sed "s/^/          /"' _ {} \;
+  find . -type f | while IFS= read -r file; do
+    target="${TARGET_DIR}/${file#./}"
+    mkdir -p "$(dirname "${target}")" || {
+      echo "        ! '${target}' -> ERROR: Failed to create directory."
+      exit 1
+    }
+    if cp "${file}" "${target}" 2>/dev/null; then
+      echo "          '${file}' -> '${target}'"
+    else
+      echo "        ! '${file}' -> ERROR: Failed to copy file to target: '${target}'"
+      exit 1
+    fi
+  done
 }
 
 remove_temporary_files() {
-  echo "      - Removing temporary files."
-  rm -rf /tmp/${GITHUB_REPO}
+  echo "      - Removing temporary files:"
+  rm -rf ${DOTFILES_DIR}
+  echo "          ${DOTFILES_DIR}"
+  echo "      - Changing back to the original directory."
+  cd -
 }
 
 # |------------------------ Script ------------------------|
 
 install() {
-  echo "Dotfiles"
-  echo "-> ${GITHUB_USER}/${GITHUB_REPO}"
+  echo " "
+  echo "+++++++ INSTALLING DOTFILES +++++++"
+  echo "from ${GITHUB_USER}/${GITHUB_REPO}"
+  echo "-----------------------------------"
+  echo " "
 
   echo "(0/3) GETTING DOTFILES"
   get_dotfiles
@@ -53,6 +94,7 @@ install() {
   echo "(2/3) CLEANING UP"
   remove_temporary_files
   echo "(3/3) DONE"
+  echo " "
 }
 
 install
